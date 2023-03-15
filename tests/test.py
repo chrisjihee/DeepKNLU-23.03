33f44f204fb959b0243a10198c6908aa41c7414b
Jihee Ryu <chrisjihee@etri.re.kr>
2023-03-15 23:28:24 +0900

    Update 1-doc_cls-train.ipynb: dataclass를 활용한 env 변수 할당!

--- tests/test.py
+++ tests/test.py
@@ -1,27 +1,44 @@
 import os
 import sys
+from dataclasses import dataclass, field, asdict
 from pathlib import Path
 
 import torch
 
-from chrisbase.io import copy_dict, get_hostname, get_hostaddr, get_working_file, working_gpus, cwd, include_cuda_dir, dirs
+from chrisbase.io import cwd, first_or, copy_dict, get_hostname, get_hostaddr, working_file, working_gpus, include_cuda_bin_dir
 from chrisbase.util import to_dataframe
-from chrisdict import AttrDict
 
-env = AttrDict()
-env.hostname = get_hostname()
-env.hostaddr = get_hostaddr()
-env.python_path = Path(sys.executable)
-env.project_name = "DeepKorean"
-env.project_path = cwd([x for x in get_working_file().parents if env.project_name in x.name][0])
-env.working_dir = Path.cwd()
-env.working_file = get_working_file().relative_to(env.project_path)
-env.working_gpus = working_gpus("0")
-env.number_of_gpus = torch.cuda.device_count()
-env.cuda_dir = include_cuda_dir()
-env.torch_cuda_ver = torch.version.cuda
+
+@dataclass
+class EnvArgs:
+    hostname: str = field(init=False)
+    hostaddr: str = field(init=False)
+    python_path: Path = field(init=False)
+    project_name: str = field()
+    project_path: Path = field(init=False)
+    working_dir: Path = field(init=False)
+    working_file: Path = field(init=False)
+    working_gpus: str = field(default="0")
+    number_of_gpus: int = field(init=False, default=0)
+    cuda_home_dir: Path = field(init=False)
+    torch_cuda_ver: str = field(init=False)
+
+    def __post_init__(self):
+        self.hostname = get_hostname()
+        self.hostaddr = get_hostaddr()
+        self.python_path = Path(sys.executable)
+        self.project_path = cwd(first_or([x for x in working_file().parents if self.project_name and x.name.startswith(self.project_name)]))
+        self.working_dir = Path.cwd()
+        self.working_file = working_file().relative_to(self.project_path)
+        self.working_gpus = working_gpus(self.working_gpus)
+        self.number_of_gpus = torch.cuda.device_count()
+        self.cuda_home_dir = include_cuda_bin_dir()
+        self.torch_cuda_ver = torch.version.cuda
+
+
+env = EnvArgs(project_name="DeepKorean", working_gpus="0,1")
 
 print(to_dataframe(copy_dict(dict(os.environ),
                              keys=[x for x in sorted(os.environ.keys()) if len(str(os.environ[x])) <= 100]),
                    columns=["os.environ", ""]))
-print(to_dataframe(env, columns=["env", ""]))
+print(to_dataframe(asdict(env), columns=["env", ""]))
