0ccd0bbda2e23de5727a7e6fcbd2561714cd8455
Jihee Ryu <chrisjihee@etri.re.kr>
2023-03-16 02:00:25 +0900

    Update util.py: - Add num_cuda_devices()

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,7 @@
-from chrisbase.util import to_dataframe
 from chrislab.common.util import GpuProjectEnv
 
-env = GpuProjectEnv(project_name="DeepKorean", working_gpus="0,1")
-print(to_dataframe(env))
+env = GpuProjectEnv(project_name="DeepKorean", working_gpus="0,1,2")
+print(env.number_of_gpus)
+
+env = GpuProjectEnv(project_name="DeepKorean", working_gpus="0")
+print(env.number_of_gpus)
