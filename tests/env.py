b32b7873f49e53c76a91421d8fdb57e16c6b0ce0
Jihee Ryu <chrisjihee@etri.re.kr>
2023-03-16 22:14:22 +0900

    Re-running tests

---
+++ tests/env.py
@@ -0,0 +1,18 @@
+from chrisbase.io import BasicProjectEnv
+from chrisbase.util import to_dataframe
+
+env = BasicProjectEnv(project_name="DeepKorean")
+print(to_dataframe(env))
+print("=" * 80)
+
+from chrisbase.util import to_dataframe
+from chrislab.common.util import GpuProjectEnv
+
+env = GpuProjectEnv(project_name="DeepKorean", working_gpus="0,1")
+print(to_dataframe(env))
+print("=" * 80)
+
+from chrisbase.io import environ_to_dataframe
+
+print(environ_to_dataframe(max_value_len=256))
+print("=" * 80)
--- tests/test.py
+++
@@ -1,7 +0,0 @@
-from chrislab.common.util import GpuProjectEnv
-
-env = GpuProjectEnv(project_name="DeepKorean", working_gpus="0,1,2")
-print(env.number_of_gpus)
-
-env = GpuProjectEnv(project_name="DeepKorean", working_gpus="0")
-print(env.number_of_gpus)
