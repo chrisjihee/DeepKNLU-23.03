6332327ae60f5b7008d46f670c3fd040389b3168
Jihee Ryu <chrisjihee@etri.re.kr>
2023-03-17 15:17:54 +0900

    Update 1-doc_cls-train.py: using 'with' clause

--- tests/1-doc_cls-train.py
+++ tests/1-doc_cls-train.py
@@ -1,26 +1,22 @@
-from chrisbase.io import out_hr, out_table
-from chrisbase.util import to_dataframe
 from chrislab.common.util import GpuProjectEnv
 from chrislab.ratsnlp import cli
 from ratsnlp.nlpbook.classification import ClassificationTrainArguments
 
-env = GpuProjectEnv(project_name="DeepKorean", working_gpus="0")
-env.config_file.write_text(ClassificationTrainArguments(
-    pretrained_model_name="pretrained/KcBERT-Base",
-    downstream_corpus_root_dir="data",
-    downstream_corpus_name="nsmc",
-    downstream_model_dir="checkpoints/nsmc",
-    downstream_model_filename="{epoch}-{val_loss:.3f}-{val_acc:.3f}",
-    monitor="max val_acc",
-    learning_rate=5e-5,
-    max_seq_length=128,
-    batch_size=200,
-    save_top_k=2,
-    epochs=3,
-    seed=7,
-).to_json(ensure_ascii=False, indent=2, default=str))
-if env.running_file.suffix == '.py':
-    out_table(to_dataframe(env, columns=[env.__class__.__name__, "value"]))
-    out_hr(c='-')
+with GpuProjectEnv(project_name="DeepKorean", working_gpus="0") as env:
+    args = ClassificationTrainArguments(
+        pretrained_model_name="pretrained/KcBERT-Base",
+        downstream_corpus_root_dir="data",
+        downstream_corpus_name="nsmc",
+        downstream_model_dir="checkpoints/nsmc",
+        downstream_model_filename="{epoch}-{val_loss:.3f}-{val_acc:.3f}",
+        monitor="max val_acc",
+        learning_rate=5e-5,
+        max_seq_length=128,
+        batch_size=200,
+        save_top_k=2,
+        epochs=3,
+        seed=7,
+    )
+    env.config_file.write_text(args.to_json(ensure_ascii=False, indent=2, default=str))
 
 cli.train(env.config_file)
