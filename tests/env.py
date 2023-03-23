4f43be43f3ab821959edc208e73218137dc0e57d
Jihee Ryu <chrisjihee@etri.re.kr>
2023-03-23 17:53:32 +0900

    Move gpus.ipynb -> gpu.ipynb - No CUDA is possible

--- tests/env.py
+++ tests/env.py
@@ -1,12 +1,8 @@
 from chrisbase.io import environ_to_dataframe
 from chrisbase.util import to_dataframe
-from chrislab.common.util import BaseProjectEnv, GpuProjectEnv
+from chrislab.common.util import BaseProjectEnv
 
 env = BaseProjectEnv(project_name="DeepKorean")
-print(to_dataframe(env))
-print("=" * 80)
-
-env = GpuProjectEnv(project_name="DeepKorean", working_gpus="0,1")
 print(to_dataframe(env))
 print("=" * 80)
 
