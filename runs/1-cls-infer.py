0dc403ca2844d8a62e2941b865830c476eb43f81
Jihee Ryu <chrisjihee@etri.re.kr>
2023-05-08 23:55:40 +0900

    Update ClassificationArguments: - working_config_file set according to run type

--- runs/1-cls-infer.py
+++ runs/1-cls-infer.py
@@ -5,9 +5,9 @@ from ratsnlp.nlpbook.classification.arguments import ClassificationDeployArgumen
 config = ClassificationDeployArguments(
     env=ProjectEnv(project="DeepKorNLU"),
     pretrained_model_path="model/pretrained/KcBERT-Base",
-    downstream_model_home="model/finetuned/nsmc",
+    downstream_model_home="model/finetuned/nsmc-0508",
     downstream_model_file=None,
-    max_seq_length=128,
+    max_seq_length=64,
 ).save_working_config()
 
 cli.serve_cls(config)
