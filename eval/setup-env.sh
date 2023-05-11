8d9d35e869669e704948af25142240c1750323d1
Jihee Ryu <chrisjihee@etri.re.kr>
2023-05-11 15:44:06 +0900

    Add setup-env.sh

---
+++ eval/setup-env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Tested on Ubuntu 20.04
+export PROJECT_NAME="DeepKorNLU-klue"
+export PYTHON_VER="3.7"
+conda update -n base -c defaults conda -y
+conda create -n $PROJECT_NAME python=$PYTHON_VER -y
+conda activate $PROJECT_NAME
+pip install -r requirements.txt
+pip install importlib-metadata==4.13.0
