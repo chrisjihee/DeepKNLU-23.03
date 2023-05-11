b94a871489c7920b68807a4a3b3a551a604fc0a4
Jihee Ryu <chrisjihee@etri.re.kr>
2023-05-11 18:03:35 +0900

    Update eval/setup-env.sh

--- eval/setup-env.sh
+++ eval/setup-env.sh
@@ -5,6 +5,8 @@ export PYTHON_VER="3.7"
 conda update -n base -c defaults conda -y
 conda create -n $PROJECT_NAME python=$PYTHON_VER -y
 conda activate $PROJECT_NAME
-pip install --upgrade torch==1.7.0 --index-url https://download.pytorch.org/whl/cu110
+
+pip install dataclasses
+pip install torch==1.7.0 --index-url https://download.pytorch.org/whl/cu110
 pip install -r requirements.txt
 
