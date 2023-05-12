8e321dfe6be18d236832a3a614fda0ec2a263587
Jihee Ryu <chrisjihee@etri.re.kr>
2023-05-13 08:35:06 +0900

    * Add runs/2-valid_ner.py

---
+++ runs/2-valid_ner.py
@@ -0,0 +1,22 @@
+from chrisbase.io import ProjectEnv
+from chrisbase.time import now
+from chrislab.ratsnlp import cli
+from ratsnlp.nlpbook.arguments import NLUTesterArguments
+
+config = NLUTesterArguments(
+    env=ProjectEnv(project="DeepKorNLU", running_gpus="0"),
+    pretrained_model_path="model/pretrained-com/KcBERT-Base",
+    downstream_model_home=f"model/finetuned/kmou-ner-{now('%m%d')}",
+    downstream_model_file="{epoch}, {val_loss:.3f}, {val_acc:.3f}",
+    downstream_task_name="ner",
+    downstream_data_home="data",
+    downstream_data_name="kmou-ner",
+    learning_rate=5e-5,
+    max_seq_length=50,
+    batch_size=100,
+    precision=16,
+    epochs=1,
+    seed=7,
+).save_working_config()
+
+cli.valid_ner(config)
