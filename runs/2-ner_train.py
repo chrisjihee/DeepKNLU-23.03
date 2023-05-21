7c098ac2e57edcb97d833807854750aee98413e0
Jihee Ryu <chrisjihee@etri.re.kr>
2023-05-22 07:25:58 +0900

    * Update NERTask:   - Update validation_step(), validation_epoch_end(): Opt1으로 klue_ner_entity_macro_f1() 계산함!

--- runs/2-ner_train.py
+++ runs/2-ner_train.py
@@ -5,12 +5,10 @@ args = TrainerArguments(
     env=ProjectEnv(
         project="DeepKorNLU",
         running_gpus="0",
-        # on_debugging=True,
-        # on_tracing=True,
     ),
     data=DataOption(
         home="data",
-        name="klue-ner-mini",
+        name="klue-ner",
         files=DataFiles(
             train="klue-ner-v1.1_train.jsonl",
             valid="klue-ner-v1.1_dev.jsonl"
@@ -21,22 +19,19 @@ args = TrainerArguments(
     model=ModelOption(
         pretrained="model/pretrained-com/KcBERT-Base",
         finetuning_home="model/finetuning",
-        finetuning_name="{epoch:02d}, {step:04d}, {train_loss:.4f}, {train_acc:.4f}, {val_loss:.4f}, {val_acc:.4f}",
+        finetuning_name="{epoch:02d}, {step:04d}, {val_loss:.3f}, {val_acc:.3f}, {val_chr_f1:.3f}, {val_ent_f1:.3f}",
         max_seq_length=50,
-        # max_seq_length=7,
     ),
     hardware=HardwareOption(
         accelerator="gpu",
-        # batch_size=100,
-        batch_size=20,
+        batch_size=100,
         precision=16,
     ),
     learning=LearningOption(
-        condition="min val_loss",
-        log_steps=50,
-        # log_steps=3,
+        condition="max val_chr_f1",
+        log_steps=20,
         num_save=10,
-        epochs=1,
+        epochs=3,
         speed=5e-5,
         seed=7,
     ),
