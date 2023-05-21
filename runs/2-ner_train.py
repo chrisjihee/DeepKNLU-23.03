421f1b1f78938e6fb915a98c6ca35ba52794123b
Jihee Ryu <chrisjihee@etri.re.kr>
2023-05-22 02:27:39 +0900

    * Update NERTask.validation_step(): 토큰 단위 예측/정답을 문자 단위 예측/정답으로 변환!

--- runs/2-ner_train.py
+++ runs/2-ner_train.py
@@ -2,7 +2,12 @@ from nlpbook.arguments import *
 from nlpbook.ner import cli
 
 args = TrainerArguments(
-    env=ProjectEnv(project="DeepKorNLU", running_gpus="0", debugging=False, tracing=False),
+    env=ProjectEnv(
+        project="DeepKorNLU",
+        running_gpus="0",
+        on_debugging=True,
+        # on_tracing=True,
+    ),
     data=DataOption(
         home="data",
         name="klue-ner-mini",
@@ -17,16 +22,19 @@ args = TrainerArguments(
         pretrained="model/pretrained-com/KcBERT-Base",
         finetuning_home="model/finetuning",
         finetuning_name="{epoch:02d}, {step:04d}, {train_loss:.4f}, {train_acc:.4f}, {val_loss:.4f}, {val_acc:.4f}",
-        max_seq_length=7,
+        max_seq_length=50,
+        # max_seq_length=7,
     ),
     hardware=HardwareOption(
         accelerator="gpu",
-        batch_size=2,  # 100,
+        # batch_size=100,
+        batch_size=20,
         precision=16,
     ),
     learning=LearningOption(
-        condition="max val_acc",
-        log_steps=3,  # 10,
+        condition="min val_loss",
+        log_steps=50,
+        # log_steps=3,
         num_save=10,
         epochs=1,
         speed=5e-5,
