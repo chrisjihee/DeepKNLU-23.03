d474a8ebb8b14d1703e5e137a03774da92c462fc
Jihee Ryu <chrisjihee@etri.re.kr>
2023-05-22 07:31:54 +0900

    * Update NERTask:   - Update training_step(): Opt1 적용!

--- runs/2-ner_train.py
+++ runs/2-ner_train.py
@@ -20,7 +20,7 @@ args = TrainerArguments(
         pretrained="model/pretrained-com/KcBERT-Base",
         finetuning_home="model/finetuning",
         finetuning_name="{epoch:02d}, {step:04d}, {val_loss:.3f}, {val_acc:.3f}, {val_chr_f1:.3f}, {val_ent_f1:.3f}",
-        max_seq_length=50,
+        max_seq_length=64,
     ),
     hardware=HardwareOption(
         accelerator="gpu",
@@ -29,7 +29,7 @@ args = TrainerArguments(
     ),
     learning=LearningOption(
         condition="max val_chr_f1",
-        log_steps=20,
+        log_steps=50,
         num_save=10,
         epochs=3,
         speed=5e-5,
