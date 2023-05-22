42b588c68f01bc7cf51bc2575b6149d4d2437005
Jihee Ryu <chrisjihee@etri.re.kr>
2023-05-22 16:09:15 +0900

    Update minor (2)

--- runs/2-ner_train.py
+++ runs/2-ner_train.py
@@ -17,6 +17,9 @@ args = TrainerArguments(
     ),
     model=ModelOption(
         pretrained="model/pretrained-pro/ETRI-RoBERTa-Base-bbpe23.03",
+        # pretrained="model/pretrained-pro/ETRI-RoBERTa-Base-bbpe22.07",
+        # pretrained="model/pretrained-com/KPF-BERT-Base",
+        # pretrained="model/pretrained-com/KLUE-BERT-Base",
         finetuning_home="model/finetuning",
         finetuning_name="{epoch:02d}, {step:04d}, {val_loss:.3f}, {val_acc:.3f}, {val_chr_f1:.3f}, {val_ent_f1:.3f}",
         max_seq_length=128,
@@ -29,8 +32,8 @@ args = TrainerArguments(
     learning=LearningOption(
         condition="max val_chr_f1",
         log_steps=50,
-        num_save=10,
-        epochs=5,
+        num_save=3,
+        epochs=10,
         speed=5e-5,
         seed=7,
     ),
