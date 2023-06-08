6bc40a049e824eca03a140560e4b44d12aa0c46c
Jihee Ryu <chrisjihee@etri.re.kr>
2023-06-08 23:28:26 +0900

    Add runs/3-dp_train.py

--- runs/3-dp_train.py
+++ runs/3-dp_train.py
@@ -13,19 +13,19 @@ run_options = {
     "6": "model/pretrained-pro/ETRI-RoBERTa-Base-bbpe23.03",
     "7": "model/pretrained-pro/ETRI-RoBERTa-Base-bbpe22.07",
 }
-run_options["ner_train"] = run_options["0"]
+run_options["dp_train"] = run_options["0"]
 assert opt in run_options, f"opt(={opt}) is not in {list(run_options.keys())}"
 
-for learning_rate in [5e-5, 4e-5, 3e-5, 2e-5, 1e-5]:
+for learning_rate in [5e-5]:  # , 4e-5, 3e-5, 2e-5, 1e-5]:
     args = TrainerArguments(
         job=JobTimer(name=f"from-{Path(run_options[opt]).stem}"),
         env=ProjectEnv(project="DeepKorNLU", running_gpus=opt),
         data=DataOption(
             home="data",
-            name="klue-ner",
+            name="klue-dp-mini",
             files=DataFiles(
-                train="klue-ner-v1.1_train.jsonl",
-                valid="klue-ner-v1.1_dev.jsonl",
+                train="klue-dp-v1.1_train.tsv",
+                valid="klue-dp-v1.1_dev.tsv",
             ),
             redownload=False,
             show_examples=0,
@@ -44,18 +44,14 @@ for learning_rate in [5e-5, 4e-5, 3e-5, 2e-5, 1e-5]:
         learning=LearningOption(
             validating_fmt="loss={val_loss:06.4f}, f1c={val_f1c:05.2f}, f1e={val_f1e:05.2f}",
             validating_on=1 / 10,
-            num_keeping=5,
+            num_keeping=2,
             keeping_by="max val_f1c",
-            epochs=10,
+            epochs=2,
             speed=learning_rate,
             seed=7,
         ),
     )
     job_name = args.job.name
-
-    args.job.name = job_name + f"-using-pl-lr={learning_rate}"
-    with ArgumentsUsing(args) as args_file:
-        cli.train(args_file)
 
     args.job.name = job_name + f"-using-fabric1-lr={learning_rate}"
     with ArgumentsUsing(args) as args_file:
